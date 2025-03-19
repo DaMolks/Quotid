d922d5ebb16cf8e331a473dbfe05120053901748
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-03-19 19:41:04 +0100

    Mise à jour de MainApplication pour initialiser Firebase

--- android/app/src/main/java/com/quotid/MainApplication.kt
+++ android/app/src/main/java/com/quotid/MainApplication.kt
@@ -1,6 +1,7 @@
 package com.quotid
 
 import android.app.Application
+import android.util.Log
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
@@ -11,8 +12,10 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
+import com.google.firebase.FirebaseApp
+import androidx.multidex.MultiDexApplication
 
-class MainApplication : Application(), ReactApplication {
+class MainApplication : MultiDexApplication(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost =
       object : DefaultReactNativeHost(this) {
@@ -41,6 +44,18 @@ class MainApplication : Application(), ReactApplication {
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
+    }
+    
+    // Initialiser Firebase
+    try {
+      if (FirebaseApp.getApps(this).isEmpty()) {
+        FirebaseApp.initializeApp(this)
+        Log.d("MainApplication", "Firebase a été initialisé avec succès")
+      } else {
+        Log.d("MainApplication", "Firebase était déjà initialisé")
+      }
+    } catch (e: Exception) {
+      Log.e("MainApplication", "Erreur lors de l'initialisation de Firebase: ${e.message}", e)
     }
   }
 }
